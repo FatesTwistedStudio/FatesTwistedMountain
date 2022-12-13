45d43328da5307856113c81073feb4be5a53e277
Wilson Castillo <72571449+XpWilson@users.noreply.github.com>
2022-12-13 13:38:00 -0500

    update
    Playground level should be good for showcase.
    Added snow particles to the hoverbord depending on movement.
    Adjusting movement speed a bit

--- Assets/Hoverboard/S_HoverboardPhysic.cs
+++ Assets/Hoverboard/S_HoverboardPhysic.cs
@@ -5,72 +5,83 @@ public class S_HoverboardPhysic : MonoBehaviour
     Rigidbody rb;
     public float horizontalTippingAlert;
     public float verticalTippingAlert;
+    public float Height;
 
     [Header ("Movement")]
-    public float Height;
-    public float jumpForce;
-    public float moveForce, turnTorque;
+    public float maxSpeed;
+    [SerializeField]
+    public float moveForce;
+    public float turnTorque;
+
+    public ParticleSystem sp;
+    public float groundRate = 5.0f;
+    public float airRate = 0;
 
     [Header ("Anchors")]
     public Transform[] anchors = new Transform[4];
     RaycastHit[] hits = new RaycastHit[4];
 
     private bool inAir;
-    private Vector3 airVelocity;
+    public float jumpForce;
+
     private float gravity = -9.81f;
     [SerializeField]
     private float gravityMultiplyer;
-    public float minRotX;
-    public float minRotY;
-    public float minRotZ;
-    public float maxRotX;
-    public float maxRotY;
-    public float maxRotZ;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
     }
 
     void Update()
     {
-        Debug.Log(inAir);
         LimitRotation();
-        ApplyGravity();
+
+        if (inAir == true)
+        {
+            rb.AddForce(transform.up * (gravity * gravityMultiplyer));
+            //Debug.Log("Applying Gravity");
+
+        }
     }
 
     void FixedUpdate()
     {
+
         for (int i = 0; i < 4; i++)
         {
             ApplyForce(anchors[i], hits[i]);
         }
-        rb.AddForce(Input.GetAxis("Vertical") * moveForce * transform.right, ForceMode.VelocityChange);
+        rb.AddForce(Input.GetAxis("Vertical") * moveForce * transform.right, ForceMode.VelocityChange );
         rb.AddTorque(Input.GetAxis("Horizontal") * turnTorque * transform.up, ForceMode.VelocityChange);
-        
+
+
         if (inAir == false && Input.GetButton("Jump"))
         {
-            rb.AddForce( transform.up * jumpForce, ForceMode.VelocityChange);      
+            rb.AddForce(transform.up * jumpForce, ForceMode.VelocityChange);
+            Debug.LogWarning("Player has pressed Jump");
+            inAir = true;
         }
-        if (inAir == true)
-        {
-            rb.AddForce(-airVelocity);
-        }    
 
     }
    public void ApplyForce(Transform anchor, RaycastHit hit)
     {
+        var emission = sp.emission;
+
         if (Physics.Raycast(anchor.position, -anchor.up, out hit, Height))
         {
             float force = 0;
             force = Mathf.Abs(1 / (hit.point.y - anchor.position.y));
             rb.AddForceAtPosition(transform.up * force * Height, anchor.position, ForceMode.Acceleration);
             inAir = false;
+            emission.rateOverDistance = groundRate;
+            
         }
         else
         {
+            emission.rateOverDistance = airRate;
             inAir = true;
-            
         }
     }
     public void overboardControls()
@@ -100,19 +111,5 @@ public class S_HoverboardPhysic : MonoBehaviour
 
 
         gameObject.transform.rotation = Quaternion.Euler(playerEulerAngles);
-    }
-
-    private void ApplyGravity()
-    {
-        if (inAir)
-        {
-            airVelocity.y += gravity * gravityMultiplyer * Time.deltaTime;
-        }
-        else
-        {
-            airVelocity.y = -1f;
-        }
-
-        
     }
 }
