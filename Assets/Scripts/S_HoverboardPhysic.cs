18e61bea00fe26b0755f29b746482176f5ca2d05
Wilson Castillo <72571449+XpWilson@users.noreply.github.com>
2022-12-16 12:58:45 -0500

    update
    😃 i hate programming
    added gravity and a better ground detection

--- Assets/Scripts/S_HoverboardPhysic.cs
+++ Assets/Scripts/S_HoverboardPhysic.cs
@@ -1,7 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
 public class S_HoverboardPhysic : MonoBehaviour
 {
+    [SerializeField]
+    Transform orientation;
     Rigidbody rb;
     public float horizontalTippingAlert;
     public float verticalTippingAlert;
@@ -12,11 +16,21 @@ public class S_HoverboardPhysic : MonoBehaviour
     [SerializeField]
     public float moveForce;
     public float turnTorque;
+    private float horizontalMovement;
+    private float verticalMovement;
+    Vector3 moveDirection;
+    
+
 
     public ParticleSystem sp;
     public AudioSource snowboardSFX;
     private bool playedAudio;
     private bool m_Play;
+
+    [SerializeField]
+    private LayerMask Ground;
+    [SerializeField]
+    private float distanceToGround;
 
     public float groundRate = 5.0f;
     public float airRate = 0;
@@ -25,12 +39,48 @@ public class S_HoverboardPhysic : MonoBehaviour
     public Transform[] anchors = new Transform[4];
     RaycastHit[] hits = new RaycastHit[4];
 
+    [Header("Drag")]
+    [SerializeField]
+    private float groundDrag = 3f;
+    [SerializeField]
+    private float groundAngDrag = 3f;
+    [SerializeField]
+    private float airDrag = 1f;
+    [SerializeField]
+    private float airAngDrag = 1f;
+
+
     private bool inAir;
+    private bool isGrounded;
     public float jumpForce;
 
-    private float gravity = -9.81f;
+    private float gravity = -20;
+    private float groundedGravity = -5f;
+    
+    [SerializeField]
+    private float maxFallSpeed;
     [SerializeField]
     private float gravityMultiplyer;
+
+    Vector3 slopeDirection;
+    RaycastHit slopeHit;
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, Height / 2 + 0.5f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    float _verticalSpeed;
 
     void Start()
     {
@@ -41,37 +91,36 @@ public class S_HoverboardPhysic : MonoBehaviour
     void Update()
     {
         LimitRotation();
+        myInput();
+        HandleDrag();
 
-        if (inAir == true)
+        if (Input.GetButton("Jump") && isGrounded)
         {
-            rb.AddForce(transform.up * (gravity * gravityMultiplyer));
-            //Debug.Log("Applying Gravity");
-
+            Jump();
         }
-
+        
+        if (!isGrounded)
+        {
+            ApplyGravity();
+        }
+        slopeDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
     }
 
     void FixedUpdate()
     {
+        isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, 0, 0), distanceToGround, Ground);
+        MovePlayer();
 
         for (int i = 0; i < 4; i++)
         {
             ApplyForce(anchors[i], hits[i]);
         }
-        rb.AddForce(Input.GetAxis("Vertical") * moveForce * transform.right, ForceMode.VelocityChange );
-        rb.AddTorque(Input.GetAxis("Horizontal") * turnTorque * transform.up, ForceMode.VelocityChange);
-
-
-        if (inAir == false && Input.GetButton("Jump"))
-        {
-            rb.AddForce(transform.up * jumpForce, ForceMode.VelocityChange);
-            //Debug.LogWarning("Player has pressed Jump");
-            inAir = true;
-        }
 
     }
    public void ApplyForce(Transform anchor, RaycastHit hit)
     {
+        
+
         var emission = sp.emission;
 
         if (Physics.Raycast(anchor.position, -anchor.up, out hit, Height))
@@ -80,8 +129,6 @@ public class S_HoverboardPhysic : MonoBehaviour
             force = Mathf.Abs(1 / (hit.point.y - anchor.position.y));
             rb.AddForceAtPosition(transform.up * force * Height, anchor.position, ForceMode.Acceleration);
             inAir = false;
-
-
             emission.rateOverDistance = groundRate;
             
         }
@@ -111,6 +158,33 @@ public class S_HoverboardPhysic : MonoBehaviour
         }
     }
 
+    private void MovePlayer()
+    {
+        if (isGrounded && !OnSlope())
+        {
+            rb.AddForce(moveDirection.normalized * moveForce * maxSpeed, ForceMode.VelocityChange);
+            rb.AddTorque(horizontalMovement * turnTorque * transform.up, ForceMode.VelocityChange);
+        }
+        else if (isGrounded && OnSlope())
+        {
+            rb.AddForce(slopeDirection.normalized * moveForce * maxSpeed, ForceMode.VelocityChange);
+            rb.AddTorque(horizontalMovement * turnTorque * transform.up, ForceMode.VelocityChange);
+        }
+        else if (!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveForce * maxSpeed * airRate, ForceMode.VelocityChange);
+            rb.AddTorque(horizontalMovement * turnTorque * transform.up, ForceMode.VelocityChange);
+        }
+
+    }
+
+    private void Jump()
+    {
+
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.VelocityChange);
+    }
+
     private void LimitRotation()
     {
         Vector3 playerEulerAngles = gameObject.transform.rotation.eulerAngles;
@@ -119,4 +193,39 @@ public class S_HoverboardPhysic : MonoBehaviour
 
         gameObject.transform.rotation = Quaternion.Euler(playerEulerAngles);
     }
+    private void HandleDrag()
+    {
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+            rb.angularDrag = groundAngDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+            rb.angularDrag = airAngDrag;
+        }
+    }
+
+    private void myInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = orientation.forward * -horizontalMovement + orientation.right * verticalMovement;
+    }
+
+    private void ApplyGravity()
+    {
+        rb.AddForce(Physics.gravity, ForceMode.Acceleration );
+        Debug.Log("Applying Gravity");
+    }
+
+    /* This wasa for debugging ground checksphere
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position - new Vector3(0, 0, 0), distanceToGround);
+    }
+    */
+
 }
