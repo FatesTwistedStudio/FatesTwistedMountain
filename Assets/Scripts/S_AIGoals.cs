a73d0b63487b2f53d52ae9fa2380793f837a155d
tncahill <97319232+tncahill@users.noreply.github.com>
2023-02-15 08:06:39 -0600

    update
    worked on script

--- Assets/Scripts/S_AIGoals.cs
+++ Assets/Scripts/S_AIGoals.cs
@@ -4,20 +4,118 @@ using UnityEngine;
 
 public class S_AIGoals : MonoBehaviour
 {
-public void AiSlime()
-    {
+    public GameObject aiChar;
+    public string aiText;
+    public S_AiMovement S_AiMovement;
 
+    private void Update()
+    {
+        if (aiText == "")
+        {
+
+        }
+        if (aiText == "Slime")
+        {
+            AiSlime();
+        }
+        if (aiText == "Larry")
+        {
+            AiLarry();
+        }
+        if (aiText == "JeroyLenkins")
+        {
+            AiJeroyLenkins();
+        }
+        if (aiText == "RickyBobby")
+        {
+            AiRickyBobby();
+        }
+    }
+    public void AiSlime()
+    {
+        // easiest
+
+        // will go for red and green flags and use them
+        if (aiChar.GetComponent<S_CharInfoHolder>() != null)
+        {
+            if (aiChar.GetComponent<S_CharInfoHolder>().itemHeld.tag == "RedFlag")
+            {
+                S_AiMovement.useItem(aiChar.GetComponent<S_CharInfoHolder>().itemHeld);
+
+            }
+            if (aiChar.GetComponent<S_CharInfoHolder>().itemHeld.tag == "GreenFlag")
+            {
+                S_AiMovement.useItem(aiChar.GetComponent<S_CharInfoHolder>().itemHeld);
+            }
+        }
+        // eager for points will use every oporunioty for it(Ie doing tricks and items at the cost of speed and time)
+
+        //lower mass
     }
     public void AiLarry()
     {
-        
+        //medium
+
+        //avoid red and green flags
+        if (aiChar.GetComponent<S_CharInfoHolder>() != null)
+        {
+            if (aiChar.GetComponent<S_CharInfoHolder>().itemHeld.tag == "RedFlag")
+            {
+                S_AiMovement.discardItem(aiChar.GetComponent<S_CharInfoHolder>().itemHeld);
+
+            }
+            if (aiChar.GetComponent<S_CharInfoHolder>().itemHeld.tag == "GreenFlag")
+            {
+                S_AiMovement.discardItem(aiChar.GetComponent<S_CharInfoHolder>().itemHeld);
+
+            }
+        }
+        //not serious about points(not going to do tricks)
+
+        //just get to the goal
     }
     public void AiJeroyLenkins()
     {
+        //hard
 
+        //avoid red flags but will use green flags
+        if (aiChar.GetComponent<S_CharInfoHolder>() != null)
+        {
+            if (aiChar.GetComponent<S_CharInfoHolder>().itemHeld.tag == "RedFlag")
+            {
+                S_AiMovement.discardItem(aiChar.GetComponent<S_CharInfoHolder>().itemHeld);
+
+            }
+            if (aiChar.GetComponent<S_CharInfoHolder>().itemHeld.tag == "GreenFlag")
+            {
+                S_AiMovement.useItem(aiChar.GetComponent<S_CharInfoHolder>().itemHeld);
+
+            }
+        }
+        //will get point where it can(ie doing tricks)
+
+        //fastest
     }
     public void AiRickyBobby()
     {
+        //expert
 
+        //Will use red flag items and avoid green flag items
+        if (aiChar.GetComponent<S_CharInfoHolder>() != null)
+        {
+            if (aiChar.GetComponent<S_CharInfoHolder>().itemHeld.tag == "RedFlag")
+            {
+                S_AiMovement.useItem(aiChar.GetComponent<S_CharInfoHolder>().itemHeld);
+
+            }
+            if (aiChar.GetComponent<S_CharInfoHolder>().itemHeld.tag == "GreenFlag")
+            {
+                S_AiMovement.discardItem(aiChar.GetComponent<S_CharInfoHolder>().itemHeld);
+
+            }
+        }
+        //will do tricks
+
+        //will try to make the player lose using items like a menace
     }
 }
--- Assets/Scripts/S_AiMovement.cs
+++ Assets/Scripts/S_AiMovement.cs
@@ -1,42 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class S_AiMovement : MonoBehaviour
 {
-    //character rigidbody
-    //navMesh components
+    
+    public NavMeshAgent aiCharacter;
+    
     public float Speed;
 
     //focus on items
+    private void Update()
+    {
+        if(gameObject.tag == "Character")
+        {
+
+        }
+    }
     public void seeItem()
     {
-        
+        Debug.Log(gameObject.name + " seees an item");
     }
-    public void useItem()
+    public void useItem(GameObject itemToUse)
     {
+        Debug.Log(gameObject.name + " uses an item");
+    }
+    public void discardItem(GameObject itemToDiscard)
+    {
+        Debug.Log(gameObject.name + " discards an item");
 
     }
     //focus on shortest path
     public void leanLeft()
     {
+        Debug.Log(gameObject.name + " leans left");
 
     }
     public void leanRight()
     {
+        Debug.Log(gameObject.name + " leans right");
 
     }
     public void leanBack()
     {
+        Debug.Log(gameObject.name + " leans back");
 
     }
     public void leanFoward()
     {
+        Debug.Log(gameObject.name + " leans foward");
 
     }
     public void doTricks()
     {
+        Debug.Log(gameObject.name + " does a trick");
 
     }
-    
+    public void jumpOnce()
+    {
+        Debug.Log(gameObject.name + " leans left");
+
+    }
 }
