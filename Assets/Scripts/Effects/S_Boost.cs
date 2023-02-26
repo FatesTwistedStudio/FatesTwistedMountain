82a03337e52d0f05e24858949926663b30e47089
SS-Viper <gperez1@student.fullsail.edu>
2023-02-26 17:12:17 -0500

    update

--- Assets/Scripts/Effects/S_Boost.cs
+++ Assets/Scripts/Effects/S_Boost.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class S_Boost : MonoBehaviour
 {
     public Vector3 boostAmount;
+    public float normalSpeed;
+    public float boostedSpeed;
+    public float speedCoolDown;
+    public float speed;
+
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
@@ -12,12 +17,22 @@ public class S_Boost : MonoBehaviour
         {
             //impulse force
             Debug.Log("Force Applied to Player");
+            speed = boostedSpeed;
+            StartCoroutine("SpeedDuration");
         }
         if (other.gameObject.tag == "Character")
         {
             //impulse force
             Debug.Log("Force Applied to Character");
+            speed = boostedSpeed;
+            StartCoroutine("SpeedDuration");
 
         }
+    }
+
+    IEnumerator SpeedDuration()
+    {
+        yield return new WaitForSeconds(speedCoolDown);
+        speed = normalSpeed;
     }
 }
