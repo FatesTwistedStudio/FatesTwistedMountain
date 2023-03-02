ac84911d3f52cb2da4e7be5fd1b0b8011b69c462
Wilson Castillo <72571449+XpWilson@users.noreply.github.com>
2023-03-02 10:38:11 -0500

    update
    porting over some shaders from FTT
    + new skybox for level 1

---
+++ Assets/Extra/Shaders/Skybox_Shader/FollowSun.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowSun : MonoBehaviour
+{
+    public Vector3 _SunDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+         Shader.SetGlobalVector("_SunDirection", transform.forward);
+    }
+
+    
+   
+}
