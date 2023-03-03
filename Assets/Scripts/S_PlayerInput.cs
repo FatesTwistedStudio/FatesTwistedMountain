f5957a4bd118bb9339f742e5ac063907a3c01633
Wilson Castillo <72571449+XpWilson@users.noreply.github.com>
2023-03-03 15:05:57 -0500

    update
    Imported Kayla's animations :fire:
    Got the animations working via player input script

--- Assets/Scripts/S_PlayerInput.cs
+++ Assets/Scripts/S_PlayerInput.cs
@@ -8,17 +8,26 @@ using UnityEngine.InputSystem;
 public class S_PlayerInput : MonoBehaviour
 {
     private PlayerInput playerInput;
+    [SerializeField]
+    private S_PlayerModelRef playerRefrence;
     public Vector2 _mvn;
     public Vector2 _rotmvn;
     public Vector2 _rotair;
 
     S_HoverboardPhysic _physicsSystem;
 
+    public Animator anim;
+    public GameObject playerRef;
+    int isLeaningForwardHash;
+    int isLeaningRightHash;
+    int isLeaningLeftHash;
+
     private void Awake()
     {
         _physicsSystem = GetComponent<S_HoverboardPhysic>();
-
         playerInput = GetComponent<PlayerInput>();
+        playerRefrence = GetComponentInChildren<S_PlayerModelRef>();
+
         playerInput.actions["Move"].performed += ctx => _mvn = ctx.ReadValue<Vector2>();
         playerInput.actions["Move"].canceled += ctx => _mvn = Vector2.zero;
 
@@ -27,6 +36,58 @@ public class S_PlayerInput : MonoBehaviour
 
         playerInput.actions["AirRotation"].performed += ctx => _rotair = ctx.ReadValue<Vector2>();
         playerInput.actions["AirRotation"].canceled += ctx => _rotair = Vector2.zero;
+
+    
+    }
+    private void Start()
+    {
+        anim = playerRefrence.GetComponent<Animator>();
+        isLeaningForwardHash = Animator.StringToHash("IsMovingForward");
+        isLeaningRightHash = Animator.StringToHash("IsMovingRight");
+        isLeaningLeftHash = Animator.StringToHash("IsMovingLeft");
+    }
+
+    private void Update()
+    {
+       // Debug.Log(_mvn.magnitude);
+        Debug.Log(_rotmvn);
+        handleMovement();
+    }
+    void handleMovement()
+    {
+        if (anim != null)
+        {
+            bool isLeaningForward = anim.GetBool(isLeaningForwardHash);
+            bool isLeaningRight = anim.GetBool(isLeaningRightHash);
+            bool isLeaningLeft = anim.GetBool(isLeaningLeftHash);
+
+            if (_mvn.magnitude > 0)
+            {
+                anim.SetBool("IsMovingForward", true);
+            }
+            else
+            {
+                anim.SetBool("IsMovingForward", false);
+            }
+            if (_rotmvn.x > 0.1f)
+            {
+                anim.SetBool("IsMovingRight", true);
+            }
+            else
+            {
+                anim.SetBool("IsMovingRight", false);
+            }
+            if (_rotmvn.x < -0.1f)
+            {
+                anim.SetBool("IsMovingLeft", true);
+            }
+            else
+            {
+                anim.SetBool("IsMovingLeft", false);
+            }
+        }
+        
+        
     }
 
     public void OnJump(InputValue value)
