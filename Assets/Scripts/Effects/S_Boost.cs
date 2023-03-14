d424f952cdf5c077ceff4629cf4d8dd6f6e044b4
Wilson Castillo <72571449+XpWilson@users.noreply.github.com>
2023-03-14 14:26:37 -0400

    update
    Created Speed Boost model
    -Created textures for it as well
    -Created Shader to have the boost arrows be scrollable
    -Replaced all of the boost objects in level 1 and 2 to have the models
    -Speed Boost model is also in the prefab now
    -Adjusted speed boost script to add force to the player via rigid body
    
    Level 1 changes
    -Added in speed boost models.
    -Overhauled the lighting, looks pretty vibrant now.
    -The directional light has a cloud shadow casted on the environment
    - Created shader to do the overcast effect
    
    Level 2 changes
    -Added in the speed boost model.

--- Assets/Scripts/Effects/S_Boost.cs
+++ Assets/Scripts/Effects/S_Boost.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class S_Boost : MonoBehaviour
 {
-    public Vector3 boostAmount;
-    public float normalSpeed;
-    public float boostedSpeed;
-    public float speedCoolDown;
-    public float speed;
+    private Rigidbody rb;
+    [SerializeField]
+    private float boostedSpeed;
+    private float normalSpeed;
+    private float speedCoolDown;
+    private float speed;
 
-    // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            rb = other.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * boostedSpeed, ForceMode.VelocityChange);
             //impulse force
             Debug.Log("Force Applied to Player");
             speed = boostedSpeed;
@@ -23,6 +25,8 @@ public class S_Boost : MonoBehaviour
         if (other.gameObject.tag == "Character")
         {
             //impulse force
+            rb = other.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * boostedSpeed, ForceMode.VelocityChange);
             Debug.Log("Force Applied to Character");
             speed = boostedSpeed;
             StartCoroutine("SpeedDuration");
