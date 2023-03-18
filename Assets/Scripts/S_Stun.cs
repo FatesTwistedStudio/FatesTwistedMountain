15bbd82f6e2409dbbbe7ada5ec2f1ae45b489654
SS-Viper <gperez1@student.fullsail.edu>
2023-03-18 00:33:47 -0400

    Update
    working on stun condition - trying out different methods to see whats gonna work best

--- Assets/Scripts/S_Stun.cs
+++ Assets/Scripts/S_Stun.cs
@@ -8,8 +8,12 @@ public class S_Stun : MonoBehaviour
     public float stunDuration = 5.0f;
     float stunRemaining = 0.0f;
 
+    public Transform target;
+    public float angle;
+
 
     S_Recovery S_Recovery;
+    S_HoverboardPhysic S_HoverboardPhysic;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +26,24 @@ public class S_Stun : MonoBehaviour
         {
             Debug.Log("S_Recovery not connected");
         }
+
+        if (GetComponent<S_HoverboardPhysic>() != null)
+        {
+            S_HoverboardPhysic = GetComponent<S_HoverboardPhysic>();
+        }
+        else
+        {
+            Debug.Log("S_HoverboardPhysic not connected");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        angle = Vector3.Angle(transform.position, target.position);
+        Debug.Log(angle);
+
         if (stunRemaining > 0.0f)
         {
             stunRemaining = Mathf.Max(stunRemaining - Time.deltaTime, 0.0f);
