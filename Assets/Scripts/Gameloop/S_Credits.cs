c8cbef2dd6bcea7419e14f986260f02363fa6db6
tncahill <97319232+tncahill@users.noreply.github.com>
2023-03-18 11:55:50 -0500

    update
    working on UI

--- Assets/Scripts/Gameloop/S_Credits.cs
+++ Assets/Scripts/Gameloop/S_Credits.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class S_Credits : MonoBehaviour
 {
     public S_CreditsDatabase creditsDatabase;
 
-   
+
     [SerializeField]
     public GameObject[] contributorLogo;
     public TextMeshProUGUI[] contributorName;
@@ -22,23 +24,25 @@ public class S_Credits : MonoBehaviour
     }
     public void setCredits()
     {
-        for (int i = 0; i < creditsDatabase.creditsInformation.Length-1; i++)
+        for (int i = 0; i < creditsDatabase.creditsInformation.Length; i++)
         {
-            if (creditsDatabase.creditsInformation[i] != null)
-            {
 
-                if (creditsDatabase.creditsInformation[i].stickerSprite != null)
-                    //contributorLogo[i].GetComponent<Image>().sprite = creditsDatabase.creditsInformation[i].stickerSprite;
+            //if (creditsDatabase.creditsInformation[i].stickerSprite != null)
+            //    contributorLogo[i]. = creditsDatabase.creditsInformation[i].stickerSprite;
 
-                    if (creditsDatabase.creditsInformation[i].contributorsName != null)
-                        contributorName[i].text =creditsDatabase.creditsInformation[i].contributorsName[i].ToString();
+            if (creditsDatabase.creditsInformation[i].contributorsName != null)
+                contributorName[i].GetComponent<TextMesh>().text = creditsDatabase.creditsInformation[i].contributorsName[i].ToString();
 
-                if (creditsDatabase.creditsInformation[i].contributorsRole != null)
-                    contributorRole[i].SetText("" + creditsDatabase.creditsInformation[i].contributorsRole[i]);
+            if (creditsDatabase.creditsInformation[i].contributorsRole != null)
+                contributorRole[i].SetText("" + creditsDatabase.creditsInformation[i].contributorsRole[i]);
 
-                if (creditsDatabase.creditsInformation[i].contributorsThanks != null)
-                    contributorThanks[i].SetText("" + creditsDatabase.creditsInformation[i].contributorsThanks[i]);
-            }
+            if (creditsDatabase.creditsInformation[i].contributorsThanks != null)
+                contributorThanks[i].SetText("" + creditsDatabase.creditsInformation[i].contributorsThanks[i]);
+
         }
+    }
+    public void sendToNextLevel(string nextLevel)
+    {
+        SceneManager.LoadScene(nextLevel);
     }
 }
