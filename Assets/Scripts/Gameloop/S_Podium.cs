f8427127f31a2259bda72567d5bc8e3be46a3fa2
tncahill <97319232+tncahill@users.noreply.github.com>
2023-03-19 07:12:13 -0500

    update
    credits

--- Assets/Scripts/Gameloop/S_Podium.cs
+++ Assets/Scripts/Gameloop/S_Podium.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class S_Podium : MonoBehaviour
 {
@@ -8,39 +9,63 @@ public class S_Podium : MonoBehaviour
     public GameObject podium1st;
     public GameObject podium2st;
     public GameObject podium3st;
+    public GameObject podium4st;
     public S_Credits S_Credits;
-    private void Update()
+    private void Awake()
     {
         if (player == null)
             player = GameObject.FindWithTag("GameController").GetComponent<S_GameloopController>().player;
 
+        GameObject.FindWithTag("EventController").SetActive(false);
 
         if (player != null)
         {
-            if (Input.GetKeyUp(KeyCode.R))
-            {
-                spawnPlayer(player, player.GetComponent<S_CharInfoHolder>().levelPlacement[0]);
-            }
+
+            spawnPlayer();
+
         }
     }
-    public void spawnPlayer(GameObject player, int levelPlacement)
+    public void spawnPlayer()
     {
         Debug.Log("player present");
         if (player.GetComponent<S_CharInfoHolder>().levelPlacement[0] == 1)
         {
             GameObject spawnCharacter = Instantiate(player, podium1st.transform.position, podium1st.transform.rotation) as GameObject;
+            spawnCharacter.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            spawnCharacter.GetComponent<S_HoverboardPhysic>().enabled = false;
+            spawnCharacter.GetComponent<S_SurfaceAlignment>().enabled = false;
+            spawnCharacter.GetComponent<S_HandleCinemachine>().enabled = false;
+            spawnCharacter.GetComponent<PlayerInput>().enabled = false;
         }
-      else if (player.GetComponent<S_CharInfoHolder>().levelPlacement[0] == 2)
+        else if (player.GetComponent<S_CharInfoHolder>().levelPlacement[0] == 2)
         {
             GameObject spawnCharacter = Instantiate(player, podium2st.transform.position, podium2st.transform.rotation) as GameObject;
+            spawnCharacter.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+            spawnCharacter.GetComponent<S_HoverboardPhysic>().enabled = false;
+            spawnCharacter.GetComponent<S_SurfaceAlignment>().enabled = false;
+            spawnCharacter.GetComponent<S_HandleCinemachine>().enabled = false;
+            spawnCharacter.GetComponent<PlayerInput>().enabled = false;
         }
-       else if (player.GetComponent<S_CharInfoHolder>().levelPlacement[0] == 3)
+        else if (player.GetComponent<S_CharInfoHolder>().levelPlacement[0] == 3)
         {
             GameObject spawnCharacter = Instantiate(player, podium3st.transform.position, podium3st.transform.rotation) as GameObject;
+            spawnCharacter.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+            spawnCharacter.GetComponent<S_HoverboardPhysic>().enabled = false;
+            spawnCharacter.GetComponent<S_SurfaceAlignment>().enabled = false;
+            spawnCharacter.GetComponent<S_HandleCinemachine>().enabled = false;
+            spawnCharacter.GetComponent<PlayerInput>().enabled = false;
         }
         else
         {
-            GameObject spawnCharacter = Instantiate(player, podium1st.transform.position, podium1st.transform.rotation) as GameObject;
+            GameObject spawnCharacter = Instantiate(player, podium4st.transform.position, podium4st.transform.rotation) as GameObject;
+            spawnCharacter.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+            spawnCharacter.GetComponent<S_HoverboardPhysic>().enabled = false;
+            spawnCharacter.GetComponent<S_SurfaceAlignment>().enabled = false;
+            spawnCharacter.GetComponent<S_HandleCinemachine>().enabled = false;
+            spawnCharacter.GetComponent<PlayerInput>().enabled = false;
         }
 
 
