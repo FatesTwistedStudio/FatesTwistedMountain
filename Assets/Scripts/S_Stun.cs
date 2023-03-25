a930c2c8ebf633280afef70939a6b9b6148f9e69
SS-Viper <gperez1@student.fullsail.edu>
2023-03-25 03:52:30 -0400

    stun
    works for the most part I have it  disabled right now though so I can work with measurements tomorrow.

--- Assets/Scripts/S_Stun.cs
+++ Assets/Scripts/S_Stun.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class S_Stun : MonoBehaviour
 {
 
+    Rigidbody rb;
+
+    [Header("Stun")]
     public float stunDuration = 5.0f;
     float stunRemaining = 0.0f;
 
-    public Transform target;
-    public float angle;
+
+    float minimumFall;
+    bool grounded;
 
 
     S_Recovery S_Recovery;
     S_HoverboardPhysic S_HoverboardPhysic;
+    S_PlayerInput S_PlayerInput;
 
     // Start is called before the first frame update
     void Start()
@@ -35,14 +40,51 @@ public class S_Stun : MonoBehaviour
         {
             Debug.Log("S_HoverboardPhysic not connected");
         }
+
+        if (GetComponent<S_PlayerInput>() != null)
+        {
+            S_HoverboardPhysic = GetComponent<S_PlayerInput>();
+        }
+        else
+        {
+            Debug.Log("S_PlayerInput not connected");
+        }
+
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+
+       if(grounded && rb.velocity.y == 0)
+        {
+            grounded = false;
+
+            if(minimumFall <= -2)
+            {
+                Stun();
+            }
+        }
+        else if (grounded)
+        {
+            if (rb.velocity.y < minimumFall)
+                minimumFall = rb.velocity.y;
+        }
+       if (!grounded)
+        {
+           minimumFall = 0;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        angle = Vector3.Angle(transform.position, target.position);
-        Debug.Log(angle);
+        //angle = Vector3.Angle(transform.position, target.position);
+        //Debug.Log(angle);
 
         if (stunRemaining > 0.0f)
         {
@@ -55,11 +97,19 @@ public class S_Stun : MonoBehaviour
         }
     }
 
+    void CheckGround()
+    {
+        grounded = Physics.Raycast(transform.position + Vector3.up, -Vector3.up, 1.01f);
+    }
+
+
+
 
     //stun 
     public void Stun()
     {
-        stunRemaining = stunDuration;
-    }
+           stunRemaining = stunDuration;
+           Debug.Log("Player fell" + stunRemaining);
 
+    }
 }
