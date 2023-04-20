b7211160ea544753ac4184a1b5b51c7b10fa3a5e
RRioga <rtmulvaney@student.fullsail.edu>
2023-04-20 18:58:03 -0400

    update
    sky incoming

---
+++ Assets/Scripts/S_SkyboxRando.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_SkyboxRando : MonoBehaviour
+{
+    
+	public Material skyOne;
+	public Material skyTwo;
+	public Material skyThree;
+	public Material skyFour;
+	public Material skyFive;
+
+
+	// Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox = skyOne;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
