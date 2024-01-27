2b59cd735d620f9a445bbe231c954b782bdee3cd
tncahill <97319232+tncahill@users.noreply.github.com>
2024-01-27 05:41:16 -0600

    updating

---
+++ Assets/Scripts/ASyncLoader.cs
@@ -0,0 +1,57 @@
+//Credit: https://www.youtube.com/watch?v=NyFYNsC3H8k - https://www.youtube.com/watch?v=YMj2qPq9CP8
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ASyncLoader : MonoBehaviour
+{
+    //[Header("Menu Screens")]
+    //[SerializeField] private GameObject loadingScreen;
+    //[]
+    public GameObject loadScreen;
+    public Slider slider;
+
+    public Image Background;
+    public Sprite[] BackgroundImages;
+
+    public TextMeshProUGUI LoadScreenHeaderText;
+    public string[] headers;
+    public TextMeshProUGUI LoadScreenLevelDetailText;
+    public string[] levelDetails;
+    public TextMeshProUGUI LoadScreenLoadingDetailsText;
+    public string[] loadingDetails;
+    public TextMeshProUGUI LoadScreenPercentage;
+
+    public void LoadLevelAsync(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            setLoadingScreenDetails(sceneIndex, progress);
+            yield return null;
+        }
+    }
+    public void setLoadingScreenDetails(int sceneIndex, float progress)
+    {
+        //slider.value = progress;
+        LoadScreenPercentage.SetText(progress * 100 + "%");
+
+        Background.sprite = BackgroundImages[sceneIndex];
+        LoadScreenHeaderText.SetText(headers[sceneIndex]);
+        LoadScreenLevelDetailText.SetText("Level:" + levelDetails[sceneIndex]);
+
+        int r = Random.Range(0, levelDetails.Length);
+        LoadScreenLoadingDetailsText.SetText(loadingDetails[r]);
+
+    }
+}
