5b6f3afdcf37c220663a92c20823259c2fcf2419
tncahill <97319232+tncahill@users.noreply.github.com>
2024-03-09 06:03:48 -0600

    update

---
+++ Assets/S_ChickenSwarm.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class S_ChickenSwarm : MonoBehaviour
+{
+    public Animator[] toonChickens;
+    public bool isTurningHead;
+
+    private void Awake()
+    {
+        toonChickens = GetComponentsInChildren<Animator>();
+        toonChickens[0].GetComponent<AnimatorController>()
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (isTurningHead)
+        {
+           // toonChickens[0]
+        }
+    }
+}
